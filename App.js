71d10810aa6206181f847222c27a3a87c1dffe30
Dev Shah <devshah2401@gmail.com>
2022-07-26 14:28:04 +0530

    mobile app working

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const url = "https://catfact.ninja/fact";
 
 export default function App() {
+  const [data,setData] = useState('');
+  const [isLoadingData, setisLoadingData] = React.useState(false);
+
+  function callonClick() {
+    fetch(url)
+      .then((response) =>
+        response != null ? response.json() : "Check your network"
+      )
+      .then((r) => {
+        setData(r);
+      //  setisLoadingData(false);
+      });
+  }
+  useEffect(()=> {
+    callonClick();
+  },[])
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>{data.fact + " " + data.length}</Text>
+      <Button title= "Refresh" onPress={callonClick}>
+      </Button>
+  </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
